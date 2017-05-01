8920c0aa4e97f5e4cc1adbe1916b31d8a8dabe4d
UmanShahzad <emidanrko564@hotmail.com>
2017-05-01 12:22:08 -0400

    Add proper image aspect ratios and a basic background.

--- js/Fliers.js
+++ js/Fliers.js
@@ -1,18 +1,61 @@
 'use strict';
 
-document.getElementById('fliers').innerHTML = '';
+const FLIER_COUNT = 4;
 
-for (var i = 1; i <= 4; ++i) {
+ /**
+  * FROM: http://stackoverflow.com/a/14731922
+  * Conserve aspect ratio of the orignal region. Useful when shrinking/enlarging
+  * images to fit into a certain area.
+  *
+  * @param {Number} srcWidth Source area width
+  * @param {Number} srcHeight Source area height
+  * @param {Number} maxWidth Fittable area maximum available width
+  * @param {Number} maxHeight Fittable area maximum available height
+  * @return {Object} { width, height }
+  */
+function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
+    var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
+    return { width: srcWidth*ratio, height: srcHeight*ratio };
+ }
+
+/**
+ * Adjust the size of a flier to look well on the screen.
+ * @param {Number} id The id of the flier, ranging from 1 to 4, inclusive.
+ */
+function adjustFlierSizeById(id) {
+    var flier = document.getElementById(id);
+
+    flier.width = calculateAspectRatioFit(
+        flier.naturalWidth, flier.naturalHeight,
+        (window.innerWidth / FLIER_COUNT) - FLIER_COUNT*10, window.innerHeight
+    ).width;
+
+    flier.height = calculateAspectRatioFit(
+        flier.naturalWidth, flier.naturalHeight,
+        (window.innerWidth / FLIER_COUNT) - FLIER_COUNT*10, window.innerHeight
+    ).height;
+}
+
+
+document.getElementById('fliers').innerHTML = '';
+for (var i = 1; i <= FLIER_COUNT; ++i) {
     document.getElementById('fliers').innerHTML += `
         <div class="col-lg-3">
-            <img id="flier${i}" src="../../../${i}.jpg" width="${window.innerWidth / 4}px" height="${window.innerHeight}px"/>
+            <img id="flier${i}" src="../../../${i}.jpg" />
         </div>
     `;
 }
 
+
+setTimeout(function () {
+    adjustFlierSizeById('flier1');
+    adjustFlierSizeById('flier2');
+    adjustFlierSizeById('flier3');
+    adjustFlierSizeById('flier4');
+}, 1);
+
 window.onresize = function() {
-    for (var i = 1; i <= 4; ++i) {
-        document.getElementById('flier'+i).width  = window.innerWidth / 4;
-        document.getElementById('flier'+i).height = window.innerHeight;
+    for (var i = 1; i <= FLIER_COUNT; ++i) {
+        adjustFlierSizeById(i);
     }
 }
