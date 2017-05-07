fb37ef1bbb106c38e52d5e70f7715c414c267721
UmanShahzad <emidanrko564@hotmail.com>
2017-05-07 16:28:59 -0400

    Had no need for separate raw hour and minute.

--- js/PrayTimesCustomization.js
+++ js/PrayTimesCustomization.js
@@ -15,24 +15,3 @@ var times = prayTimes.getTimes(new Date(), [40.7685, 73.9646], 5, 1, '12h');
  * NOTE: PrayerTimes.computeTimes had to be modified slightly for this to work.
  */
 var _times     = prayTimes.computeTimes();
-var fajrRaw    = getRawPrayerTimes(_times.fajr);
-var sunriseRaw = getRawPrayerTimes(_times.sunrise);
-var dhuhrRaw   = getRawPrayerTimes(_times.dhuhr);
-var asrRaw     = getRawPrayerTimes(_times.asr);
-var maghribRaw = getRawPrayerTimes(_times.maghrib);
-var ishaRaw    = getRawPrayerTimes(_times.isha);
-
-function getPrayerHour(time) {
-    return Math.floor(time);
-}
-
-function getPrayerMinute(time) {
-    return Math.ceil((time - Math.floor(time))*60);
-}
-
-function getRawPrayerTimes(time) {
-    return {
-        hour: getPrayerHour(time),
-        minute: getPrayerMinute(time)
-    };
-}
