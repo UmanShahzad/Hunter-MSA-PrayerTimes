af8cb85b6d343c064837e8b2c4ec203e405cf0c2
Uman Shahzad <emidanrko564@hotmail.com>
2017-05-09 14:51:19 -0400

    1 picture at a time.

--- skins/default/js/Fliers.js
+++ skins/default/js/Fliers.js
@@ -152,14 +152,14 @@ setTimeout(function () {
             arrows: false,
             centerMode: false,
             centerPadding: '60px',
-            slidesToShow: 2,
+            slidesToShow: 1,
             responsive: [ {
                 breakpoint: 768,
                 settings: {
                     arrows: false,
                     centerMode: true,
                     centerPadding: '40px',
-                    slidesToShow: 2
+                    slidesToShow: 1
                 }
             },
             {
